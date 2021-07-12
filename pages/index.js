0f691255eb84d7e96c2d59fec59f1fcaf1ca9178
Ankoki <72931234+Ankoki-Dev@users.noreply.github.com>
2021-07-12 17:12:53 +0100

    added social media access in the footer

--- pages/index.js
+++ pages/index.js
@@ -11,21 +11,30 @@ export default function Home() {
             <head>
                 <title>Ankoki | Jay</title>
                 <link rel="icon" href="/favicon.ico"/>
+                <meta charSet="UTF-8"/>
+                <meta name="author" content="Ankoki"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             </head>
-
-            <header className={styles.header}>
-            </header>
 
             <main className={styles.main}>
                 <h1 className={styles.title}>ANKOKI</h1>
-                <p className={styles.typewriter}>Hi, i'm Jay, I develop stuff!</p>
+                <p className={styles.typewriter}>Hi, i'm Jay, i develop stuff!</p>
             </main>
 
             <footer className={styles.footer}>
                 <a href="https://discord.gg/3RWFg2xDBF">
-                    <img src="/aspect_logo.png" alt="Join the Aspect Productions discord." className={styles.logo}/>
+                    <img src="/discord_logo.png" alt="Join the Aspect Productions discord." className={styles.logo}/>
+                </a>
+                <a href="https://github.com/Ankoki-Dev/">
+                    <img src="/github_logo.png" alt="Browse my GitHub." className={styles.logo}/>
+                </a>
+                <a href="https://open.spotify.com/user/2mauo1ot2h9agsixazyb0cps3?si=dcaab9a0c8c44e38">
+                    <img src="/spotify_logo.png" alt="See what music i like." className={styles.logo}/>
+                </a>
+                <a href="https://www.twitter.com/ankokii">
+                    <img src="/twitter_logo.png" alt="Follow my twitter." className={styles.logo}/>
                 </a>
             </footer>
         </div>
-    )
+)
 }
