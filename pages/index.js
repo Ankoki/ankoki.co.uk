3f502a79c79d58e5d85be909e220268f80e797c5
Ankoki <72931234+Ankoki-Dev@users.noreply.github.com>
2021-07-12 13:41:26 +0100

    playing around

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hey I'm kinda stupid kek
         </h1>
 
         <p className={styles.description}>
