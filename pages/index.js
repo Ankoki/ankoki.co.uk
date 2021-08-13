cf81ff9141164e42004ae2256ca2d1bcb84b7b84
Ankoki <72931234+Ankoki-Dev@users.noreply.github.com>
2021-08-13 17:55:04 +0100

    redid it because i didnt like it

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Typist from 'react-typist'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faGithub, faTwitter, faSpotify, faDiscord, faTwitch} from '@fortawesome/free-brands-svg-icons'
 
 // noinspection JSUnusedGlobalSymbols
 export default function Home() {
     return (
-        <div className={styles.container} onLoad={() => {
+        <div className={styles.container}/* onLoad={() => {
             document.body.style.opacity = '1'
             document.body.style.color = '#164A41'
-        }}>
+        }}*/>
 
             <head>
                 <title>Ankoki | Jay</title>
@@ -18,35 +21,45 @@ export default function Home() {
             </head>
 
             <main className={styles.main}>
-                <h1 className={styles.title}>ANKOKI</h1>
-                <p className={styles.typewriter}>hi, i'm jay, i develop stuff!</p>
+                <h1 className={styles.a}>A N K O K I</h1>
+                <Typist
+                    className={styles.typist}
+                    avgTypingDelay={50}
+                    cursor={{
+                        show: true,
+                        blink: true,
+                        element: "|",
+                        hideWhenDone: true,
+                        hideWhenDoneDelay: 0,
+                    }}>My name is Jay, I'm 18 and from Earth!
+                    <Typist.Delay ms={500}/>
+                    <Typist.Backspace count={6} delay={500}/>
+                    Britain!
+                    <Typist.Delay ms={500}/>
+                    <Typist.Backspace count={8} delay={500}/>
+                    <Typist.Delay ms={500}/>
+                    England!
+                </Typist>
                 <p className={styles.center}>
-                    my name is jay, i'm from england and i am 18:)<br/>
-                    i mainly like to mess around with java, usually in discord bots or<br/>
-                    minecraft plugins. when i'm not developing, i'll play games <br/>
-                    such as genshin impact, valorant or league of legends<br/>
-                    on the off chance i'll be binge watching miraculous ladybug,<br/>
-                    but thats not one of my flexes x)
+                    I am a student and enjoy developing projects in Java when I have the time!<br/>
+                    You could check out some of my recent projects by visiting my GitHub!<br/>
+                    It is linked along with my other socials at the bottom of the page.<br/>
+                    Thank you for visiting my site ^.^
                 </p>
             </main>
 
             <footer className={styles.footer}>
-                <a href="https://discord.gg/3RWFg2xDBF" target="_blank">
-                    <img src="/discord_logo.png" alt="Join the Aspect Productions discord." className={styles.logo}/>
-                </a>
-                <a href="https://github.com/Ankoki-Dev/" target="_blank">
-                    <img src="/github_logo.png" alt="Browse my GitHub." className={styles.logo}/>
-                </a>
-                <a href="https://open.spotify.com/user/2mauo1ot2h9agsixazyb0cps3?si=dcaab9a0c8c44e38" target="_blank">
-                    <img src="/spotify_logo.png" alt="See what music i like." className={styles.logo}/>
-                </a>
-                <a href="https://www.twitter.com/ankokii" target="_blank">
-                    <img src="/twitter_logo.png" alt="Follow my twitter." className={styles.logo}/>
-                </a>
-                <a href="https://www.twitch.tv/ankokii" target="_blank">
-                    <img src="/twitch_logo.png" alt="Follow my twitch." className={styles.logo}/>
-                </a>
+                <a href="https://discord.gg/3RWFg2xDBF" target="_blank" className={styles.logo}><FontAwesomeIcon
+                    icon={faDiscord}/></a>
+                <a href="https://github.com/Ankoki-Dev" target="_blank" className={styles.logo}><FontAwesomeIcon
+                    icon={faGithub}/></a>
+                <a href="https://open.spotify.com/user/2mauo1ot2h9agsixazyb0cps3?si=dcaab9a0c8c44e38" target="_blank"
+                   className={styles.logo}><FontAwesomeIcon icon={faSpotify}/></a>
+                <a href="https://www.twitter.com/ankokii" target="_blank" className={styles.logo}><FontAwesomeIcon
+                    icon={faTwitter}/></a>
+                <a href="https://www.twitch.tv/ankokii" target="_blank" className={styles.logo}><FontAwesomeIcon
+                    icon={faTwitch}/></a>
             </footer>
         </div>
-)
+    )
 }
